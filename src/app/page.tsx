c2352a80af6cd5a6dca51fe1587ff06db1963973
dianck2002 <dianck2002@gmail.com>
2025-07-02 07:54:19 +0000

    Initial commit

---
+++ src/app/page.tsx
@@ -0,0 +1,20 @@
+import Display from "./components/display";
+
+export default function Home() {
+  const name: string = "Andi";
+  const usia: number = 20;
+  return (
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
+      <div className="content">
+        ksldfaaaaaksfadkjksfdajlfdsakljls
+        sdlakf;lksad;kgl;asdkg;l
+        gdsalk;lgdsak;lkgsda
+        sadlg;lkgdsal;ks;ldag
+        ;sldakg;lkgds;lak;lgdsk;lkg
+        ;ldsakg;lkas;gd
+        lsdagl;k;lgsak
+        dsal;gk;lkgdsa;lgksa;lk;l
+      </div>
+    </div>
+  );
+}
